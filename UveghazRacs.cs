3629b789cddd23c95982cdda9a1bcc0c87b5bf16
PetrikDiak <@petrik.hu>
2026-02-26 13:49:13 +0100

    a

--- UveghazRacs.cs
+++ UveghazRacs.cs
@@ -8,5 +8,30 @@ namespace UveghazRendszer
 {
 	internal class UveghazRacs
 	{
+		int meret;
+		Cella[,] kert;
+
+		public UveghazRacs(int meret)
+		{
+			this.meret = meret;
+			this.kert = new Cella[this.meret, this.meret];
+			Parcellaz();
+		}
+
+		private void Parcellaz()
+		{
+			for (int i = 0; i < this.kert.GetLength(0); i++)
+			{
+				for (int j = 0; i < this.kert.GetLength(1); j++)
+				{
+					this.kert[i, j] = new Cella(new Pozicio(i, j));
+				}
+			}
+		}
+
+		public Cella CellaLekerdezes()
+		{
+
+		}
 	}
 }
